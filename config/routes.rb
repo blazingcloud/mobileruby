b9f3368f444931068cdafb007c266c9d7bc785a7
Sarah Allen <sarah@ultrasaurus.com>
2010-02-20 09:53:38 -0800

    remove delete and make page at root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
+  map.root :controller => 'people'
   map.resources :people
 
   # The priority is based upon order of creation: first created -> highest priority.
